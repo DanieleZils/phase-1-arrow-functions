afaeeabbfb36ffa8fe006a665d58feb4c5402e52
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-02-04 11:39:33 -0500

    arrow-functions-lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function () {
+    let a = 2000
+    let b = 100
+    return a / b
+}
+
+const square = parameter => {
+    return parameter * parameter
+}
+
+const add = (a, b) => {
+    return a + b
+}
